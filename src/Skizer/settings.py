9bc5456e455493b02872db3ee4d8cb40b5e59639
sk-nova <karimshadaab510@gmail.com>
2025-04-06 21:35:02 +0530

    Added environment variables

--- src/Skizer/settings.py
+++ src/Skizer/settings.py
@@ -3,6 +3,7 @@ Django settings for Skizer project.
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#tp#miyz7t(z_yh^ifd=fp*p&%b#zbw!b!-)%!s*ihz%1aj1ov"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [".railway.app"]
 
